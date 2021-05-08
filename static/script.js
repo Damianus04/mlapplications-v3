4d4f60772f89fa1abd0e9ba266f1207be05bd53b
Damianus Deni <damianus.deni@gmail.com>
2021-05-08 23:25:12 +0700

    update text preprocessing without stemmer

--- static/script.js
+++ static/script.js
@@ -1,35 +1,3 @@
-// const idxx = document.getElementById("idxx");
-// idxx.style.color = "green";
-
-// const input_keyword = document.querySelector(".input-keyword");
-// const input_keyword = document.querySelector(".sentiment");
-
-// function ubahWarna() {
-//   input_keyword.style.backgroundColor = "green";
-// }
-
-// input_keyword.onclick = ubahWarna;
-
-// const els = document.querySelector(".display-4");
-// els.addEventListener("click", function () {
-//   const sectiona = document.querySelector("sectiona");
-//   const h3Baru = document.createElement("div");
-//   const teksBaru = document.createTextNode("teksbaru");
-//   h3Baru.appendChild(teksBaru);
-//   sectiona.appendChild(h3Baru);
-// });
-
-// input_keyword.keydown = ubahWarna;
-
-// function untuk ubah warna - it works
-// const els = document.querySelector(".display-4");
-// function ubahWarna() {
-//   els.style.backgroundColor = "green";
-// }
-// els.onclick = ubahWarna;
-
-// PR: cara menambah elemen baru pada halaman html
-
 console.log('trial of vertical scroll table')
 
 
